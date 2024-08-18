43f8d1c9eb63a10765ae0a40eda77411f5cf928c
Agilan Hariharan <agilanhariharan@Agilans-MacBook-Pro.local>
2024-08-18 08:41:21 -0400

    solve page styles + nav links

--- src/Solve.js
+++ src/Solve.js
@@ -50,11 +50,11 @@ function Solve() {
         <div className="nav-container">
           <a className="logo">Mathify</a>
           <ul className="nav-links">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/#about">About</a></li>
             <li><Link to="/solve">Solve</Link></li>
             <li><Link to="/videos">Videos</Link></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="/#contact">Contact</a></li>
           </ul>
         </div>
       </nav>
